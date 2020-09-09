35986d7a585a5f18341d7f29f5903e2997be1d4f
Vijeth Revankar <38877972+vijethx@users.noreply.github.com>
2020-09-09 11:17:52 +0530

    Add files via upload

---
+++ build/precache-manifest.20f5a232bf37da76c0e8b0ba98824be9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cb48fdf2423f60c8cdb6a62172518cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a844e21d66661b94203",
+    "url": "/static/css/main.4c76717b.chunk.css"
+  },
+  {
+    "revision": "76f2c043ad083df2ad31",
+    "url": "/static/js/2.84229a27.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.84229a27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a844e21d66661b94203",
+    "url": "/static/js/main.7dcb3e29.chunk.js"
+  },
+  {
+    "revision": "bca2f208509ed9d2e542",
+    "url": "/static/js/runtime-main.dbd73d5b.js"
+  }
+]);
